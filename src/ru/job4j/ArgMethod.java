696ad62b6b5baf4bd05472efd652648472c8b76e
Ivan Kusurhashev <ivankusur@gmail.com>
2020-07-17 11:16:27 +0300

    4.1. Аргументы [#323241]

---
+++ src/ru/job4j/ArgMethod.java
@@ -0,0 +1,20 @@
+package ru.job4j;
+
+public class ArgMethod {
+    public static void hello(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+    public static void main(String[] args) {
+        String name = "Petr Arsentev";
+        String age = "33";
+
+        ArgMethod.hello(name);
+
+        ArgMethod.hello(age);
+
+        ArgMethod.hello(age);
+
+        ArgMethod.hello(name);
+    }
+}
