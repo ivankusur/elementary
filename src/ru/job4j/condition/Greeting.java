1ddfba108206608be65ae28e71d56d4966c0c7ef
Ivan Kusurhashev <ivankusur@gmail.com>
2020-07-17 23:45:27 +0300

    9.1. String [#323240]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        int year = 2020;
+        String idea = "I Like Java!" + year + " ";
+        System.out.println(idea + "But I am big Guy");
+
+
+    }
+}
