e3f555195d3d7f31f5c8596ad0693448ac154db2
Ivan Kusurhashev <ivankusur@gmail.com>
2020-07-20 10:47:41 +0300

    CheckStyle modification

--- src/ru/job4j/condition/TrgArea.java
+++ src/ru/job4j/condition/TrgArea.java
@@ -6,7 +6,6 @@ public class TrgArea {
         return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
 
-
     public static void main(String[] args) {
         double rsl = TrgArea.area(2, 2, 2);
         System.out.println("area (2, 2, 2) = " + rsl);
