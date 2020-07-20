1594fb162d032712a9ff17aef3dde935128b05f7
Ivan Kusurhashev <ivankusur@gmail.com>
2020-07-20 11:18:55 +0300

    2. Тесты для идеальный вес [#323270]

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -1,22 +1,22 @@
 package ru.job4j.calculator;
 
 public class Fit {
-    public static double manWeight(short height) {
+    public static double manWeight(double height) {
         return (height - 100) * 1.15;
     }
 
-    public static double womanWeight(short height) {
+    public static double womanWeight(double height) {
         return (height - 110) * 1.15;
     }
 
     public static void main(String[] args) {
-        short manheight = 177;
+        double manheight = 177;
         double man = Fit.manWeight(manheight);
         System.out.println("Man 177 is " + man);
 
-        short womanWeight = 167;
-        double wonam = Fit.womanWeight(womanWeight);
-        System.out.println("Woman 167 is " + wonam);
+        double womanWeight = 167;
+        double woman = Fit.womanWeight(womanWeight);
+        System.out.println("Woman 167 is " + woman);
 
     }
 }
---
+++ test/ru/job4j/calculator/FitTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.calculator;
+
+import org.junit.Assert;
+
+import org.junit.Test;
+
+public class FitTest {
+
+    @Test
+    public void whenHeight177ThanManWaight8855() {
+        double in = 177.0;
+        double expected = 88.55;
+        double out = Fit.manWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenHeight167ThanWomaWaight6555() {
+        double in = 167;
+        double expected = 65.55;
+        double out = Fit.womanWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
