db29901de2bd7371a57875328f114062033de0fb
Ivan Kusurhashev <ivankusur@gmail.com>
2020-07-22 16:51:37 +0300

    MaxTestFixed

--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -8,10 +8,17 @@ import static org.junit.Assert.assertThat;
 public class MaxTest {
 
     @Test
-    public void max() {
-        assertThat(
-                Max.max(10, 5),
-                is(10)
-        );
+    public void whenMax10To5Then10() {
+        assertThat(Max.max(10, 5), is(10));
+    }
+
+    @Test
+    public void whenMax7To15Then15() {
+        assertThat(Max.max(7, 15), is(15));
+    }
+
+    @Test
+    public void whenMax5To5ThenUnknown() {
+        assertThat(Max.max(5, 5), is(5));
     }
 }
