c7269ac1554e7841e0b0b94c5b496596fd7805b0
Ivan Kusurhashev <ivankusur@gmail.com>
2020-07-22 16:51:57 +0300

    MaxFixed

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -1,13 +1,16 @@
 package ru.job4j.condition;
 
-public class Max {
+public class Max<result> {
     public static int max(int first, int second) {
         boolean compare = first > second;
         int result = compare ? first : second;
+        if (first == second) {
+            result = first;
+        }
         return result;
     }
 
-    public static void main(String[] ars) {
+    public static void main(String[] args) {
         System.out.println(max(10, 5));
     }
 }
