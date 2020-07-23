089744780e1915cc5a6825371c8afb5fd02a67b5
Ivan Kusurhashev <ivankusur@gmail.com>
2020-07-23 08:02:17 +0300

    5.0.1. Сумма чисел. [#323289]FixTest

--- test/ru/job4j/loop/CounterTest.java
+++ test/ru/job4j/loop/CounterTest.java
@@ -3,13 +3,13 @@ package ru.job4j.loop;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class CounterTest {
 
     @Test
     public void sum() {
-        assertThat(Counter.sum(3, 5)),
-        is (12);
+        assertThat((Counter.sum(3, 5)),
+                is(12));
     }
 }
