ada6ca8d3ea8f775eac2d6b85562a2d4bc3005ac
Ivan Kusurhashev <ivankusur@gmail.com>
2020-07-31 19:18:56 +0300

    6.0.2. Заполнение массива. [#323215]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -1,7 +1,18 @@
 package ru.job4j.array;
 
 public class ArrayDefinition {
-    short[] ages = new short[10];
-    String[] sureName = new String[100500];
-    float[] prices = new float[40];
+    public static void main(String[] args) {
+
+        short[] ages = new short[10];
+        String[] sureName = new String[100500];
+        float[] prices = new float[40];
+
+        String[] names = new String[4];
+        names[0] = "Ivan";
+        names[1] = "Petr";
+        names[2] = "Stas";
+        names[3] = "Andrey";
+
+        System.out.println(names[0] + " " + names[1] + " " + names[2] + " " + names[3]);
+    }
 }
