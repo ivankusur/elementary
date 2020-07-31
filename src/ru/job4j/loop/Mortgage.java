5b67ea1db5fe5d319995e26ce3f78ab381e2e353
Ivan Kusurhashev <ivankusur@gmail.com>
2020-07-31 13:07:29 +0300

    5.7. Ипотека [#323286]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(int amount, int salary, int percent) {
+        int year = 0;
+
+        while (amount > 0) {
+
+            amount += amount * percent / 100;
+            amount -= salary;
+            year += 1;
+
+        }
+        return year;
+    }
+}
