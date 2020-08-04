628e49daf6a0351f421f70e3ac939c6c2f32129e
Ivan Kusurhashev <ivankusur@gmail.com>
2020-08-04 10:48:03 +0300

    6.2.2. Переставить элементы массива [#323220]

--- src/ru/job4j/array/Turn.java
+++ src/ru/job4j/array/Turn.java
@@ -1,21 +1,13 @@
 package ru.job4j.array;
 
-import java.util.Arrays;
-
 public class Turn {
     public static int[] back(int[] array) {
         for (int index = 0; index < array.length / 2; index++) {
-            int temp = (array.length - index) - 1;
-            array[temp] = array[index];
-            array[index] = temp;
+            int temp = array[index];
+            array[index] = array[(array.length - index) - 1];
+            array[(array.length - index - 1)] = temp;
         }
 
         return array;
-    }
-
-    public static void main(String[] args) {
-        int[] input = {1, 2, 3, 4, 5};
-        int[] inputBack = back(input);
-        System.out.println(Arrays.toString(inputBack));
     }
 }
