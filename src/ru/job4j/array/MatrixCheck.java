9143eb7c7cd33be9a669b2f95157d00ef368f117
Ivan Kusurhashev <ivankusur@gmail.com>
2020-08-09 12:37:27 +0300

    6.7.4. Выигрышные комбинации в сокобан [#323205]

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -30,4 +30,17 @@ public class MatrixCheck {
         }
         return rsl;
     }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if (extractDiagonal(board)[i] == 'X') {
+                if (monoHorizontal(board, i) || monoVertical(board, i)) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
 }
